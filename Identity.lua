1d24607d40718a8810d9f924653ffc5b7d610ac8
John Pavley <john@pavley.com>
2012-10-23 22:59:37 -0400

    skeleton Identity class with property and unit test

--- Identity.lua
+++ Identity.lua
@@ -1,4 +1,23 @@
-put #!/usr/bin/env lua
+#!/usr/bin/env lua
 
-Identity = { }
+Identity = {} -- the table to hold the class
+
+Identity.new = function(unique_id)
+	-- private properties
+	local self = {} -- object of class
+	unique_id = unique_id or math.random();
+	-- getters
+	-- setters
+	-- methods
+	self.show = function()
+		print("Idenity: " .. unique_id)	
+	end
+	
+	return self -- return the object!
+end
+
+-- unit tests
+
+test_identity = Identity.new("John")
+test_identity.show()
 
