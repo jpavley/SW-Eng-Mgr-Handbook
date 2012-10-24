562ce4cb9313a664c38b53c7c9501bc23d11d558
John Pavley <john@pavley.com>
2012-10-23 23:18:41 -0400

    more properties and unit tests

--- Identity.lua
+++ Identity.lua
@@ -5,12 +5,33 @@ Identity = {} -- the table to hold the class
 Identity.new = function(unique_id)
 	-- private properties
 	local self = {} -- object of class
-	unique_id = unique_id or math.random();
-	-- getters
-	-- setters
-	-- methods
+	
+	unique_id = unique_id or math.random(1000);
+	gender = "unknown"
+	age = -1 -- unknown
+	alive = true
+	personality = "unknown"
+	
+	-- public getters
+	self.get_gender = function() return gender end
+	self.get_age = function() return age end
+	self.is_alive = function() return alive end
+	self.get_personality = function() return personality end
+	
+	-- public setters
+	self.set_gender = function(g) gender = g end
+	self.set_age = function(a) age = a end
+	self.set_alive = function(a) alive = a end
+	self.set_personality = function(p) personality = p end
+
+	-- public methods
+	
 	self.show = function()
-		print("Idenity: " .. unique_id)	
+		print("Idenity: " .. unique_id 
+		.. ", Gender: " .. gender 
+		.. ", Age: " .. age 
+		.. ", Alive: " .. tostring(alive) 
+		.. ", Personality: " .. personality)	
 	end
 	
 	return self -- return the object!
@@ -20,4 +41,8 @@ end
 
 test_identity = Identity.new("John")
 test_identity.show()
+
+test_identity = Identity.new()
+test_identity.show()
+
 
