a13517d99cd7bb1a859608192346995b16d0dcf3
John Pavley <john@pavley.com>
2012-11-04 12:29:53 -0500

    finished personality class, updated unit tests

--- Personality.lua
+++ Personality.lua
@@ -19,17 +19,69 @@ Personality.new = function(unique_id)
 	
 	-- private properties
 	unique_id = unique_id or constants.kUnknownString
+	
+	-- The following five personality trait score are based on Lewis Goldberg's 
+	-- five-dimension personality model, nicknamed the "Big Five." I found this 
+	-- model more useful than "personality types" or other theories like behavorist,
+	-- social cognitive, humanistic, etc... I don't need to understand the underlying
+	-- mechansims. Traits are easy to communicate and most people feel they have some
+	-- control over their traits. This my own interpertation based on real world
+	-- experience of leading and managing people for over 20 years. Your milage may
+	-- vary :)
+	
+	openess_score = constants.kUnknownNumber 
+	-- 0 = practical, conforming, and internested in routine
+	-- 5 = will stick to plan but handles changes well, thinks outside (but not too far outside) the box
+	-- 10 = imaginitive, independent, interested in variety
+	
+	conscientiousness_score  = constants.kUnknownNumber
+	-- 0 = careless, disorganized, implusive
+	-- 5 = will take calculated risks, doesn't reply on heavy processes, communicates appropriately
+	-- 10 = organized, careful, disciplined
+	
+	extraversion_score = constants.kUnknownNumber
+	-- 0 = retiring, somber, reserved
+	-- 5 = will work seriously but able to integreate well with team, not disruptive
+	-- 10 = sociable, fun-loving, affectionate
+	
+	agreeableness_score = constants.kUnknownNumber
+	-- 0 = ruthless, suspicious, uncoorerative
+	-- 5 = gets own projects down but helpful to others, no hidden agendas
+	-- 10 = softheated, trusting, helpful
+	
+	neuroticism_score = constants.kUnknownNumber
+	-- 0 = calm, secure, self-satisfied
+	-- 5 = steady, dependible, not over confident, and only complains about real problems
+	-- 10 = anxious, insecure, self-pitying
+		
 
 	-- public getters
 	self.get_unique_id = function() return unique_id end
+	self.get_openess_score = function() return openess_score end
+	self.get_conscientiousness_score = function() return conscientiousness_score end
+	self.get_extraversion_score = function() return extraversion_score end
+	self.get_agreeableness_score = function() return agreeableness_score end
+	self.get_neuroticism_score = function() return neuroticism_score end
+
 	
 	-- public setters
 	self.set_unique_id = function(num) unique_id = num end
+	self.set_openess_score = function(num) openess_score = num end
+	self.set_conscientiousness_score = function(num) conscientiousness_score = num end
+	self.set_extraversion_score = function(num) extraversion_score = num end
+	self.set_agreeableness_score = function(num) agreeableness_score = num end
+	self.set_neuroticism_score = function(num) neuroticism_score = num end
 
 	-- public methods
 	
 	self.show = function()
-		print("Personality: " .. unique_id)
+		print("Personality: " .. unique_id
+		.. ". Openess: " .. openess_score 
+		.. ", Conscientiousness: " .. conscientiousness_score
+		.. ". Extraversion: " .. extraversion_score
+		.. ", Agreeableness: " .. agreeableness_score
+		.. ". Neuroticism: " .. neuroticism_score)
+
 	end
 		
 	return self -- return the object!
--- tests.lua
+++ tests.lua
@@ -4,16 +4,42 @@ require "Personality"
 
 -- Identity unit tests
 
-test_identity = Identity.Identity.new("111-11-111", "Charles", "Darwin")
-test_identity.set_gender(constants.kGenderMale)
-test_identity.set_middle_name("Robert")
-test_identity.set_date_of_birth(os.date("%d/%m/%y"))
-test_identity.show()
+print("--- Identity ---")
 
-test2_identity = Identity.Identity.new()
+test1_identity = Identity.Identity.new()
+test1_identity.show()
+
+test2_identity = Identity.Identity.new("111-11-111", "Charles", "Darwin")
+test2_identity.set_gender(constants.kGenderMale)
+test2_identity.set_middle_name("Robert")
+test2_identity.set_date_of_birth(os.date("%d/%m/%y"))
 test2_identity.show()
+
 
 -- Personality unit tests
 
-test_personality = Personality.Personality.new()
-test_personality.show()
+print("--- Personality ---")
+
+test1_personality = Personality.Personality.new()
+test1_personality.show()
+
+test2_personality = Personality.Personality.new("111-111-111")
+test2_personality.set_openess_score(7)
+test2_personality.set_conscientiousness_score(5)
+test2_personality.set_extraversion_score(2)
+test2_personality.set_agreeableness_score(3)
+test2_personality.set_neuroticism_score(4)
+test2_personality.show()
+
+-- Composite person tests
+
+print("--- Composite Person ---")
+
+person = {}
+
+person.identity = test2_identity
+person.personality = test2_personality
+
+person.identity.show()
+person.personality.show()
+
