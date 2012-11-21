af083c44ea2d4ddc6a495b9a22ea93fcebeca095
John Pavley <john@pavley.com>
2012-11-20 20:27:00 -0500

    Json-ized properties in Identity

--- Identity.lua
+++ Identity.lua
@@ -43,6 +43,7 @@
 
 module(..., package.seeall)
 require "constants"
+require "Json"
 
 -- ----------------------------------------------------------------------------
 -- Identity Class
@@ -59,39 +60,34 @@ Class.new = function(unique_id, first_name, last_name)
 	local self = {} -- object of class
 	
 	-- private properties
-	unique_id = unique_id or constants.kUnknownString
-	first_name = constants.kUnknownString
-	middle_name = constants.kUnknownString
-	last_name = constants.kUnknownString
-	gender = constants.kGenderUnknown
-	date_of_birth = constants.kUnknownDate
-	
-	-- public getters
-	self.get_unique_id = function() return unique_id end
-	self.get_gender = function() return gender end
-	self.get_first_name = function() return first_name end
-	self.get_last_name = function() return last_name end
-	self.get_middle_name = function() return middle_name end
-	self.get_date_of_birth = function() return date_of_birth end
+	properties  = { ["unique_id"] = unique_id or constants.kUnknownString, 
+                    ["first_name"] = first_name or constants.kUnknownString,
+                    ["middle_name"] = constants.kUnknownString,
+                    ["last_name"] = last_name or constants.kUnknownString,
+                    ["gender"] = constants.kGenderUnknown,
+                    ["date_of_birth"] = constants.kUnknownDate }
+                
+ 	-- public getters
+	self.get_unique_id = function() return properties.unique_id end
+	self.get_gender = function() return properties.gender end
+	self.get_first_name = function() return properties.first_name end
+	self.get_last_name = function() return properties.last_name end
+	self.get_middle_name = function() return properties.middle_name end
+	self.get_date_of_birth = function() return properties.date_of_birth end
 	
 	-- public setters
-	self.set_unique_id = function(num) unique_id = num end
-	self.set_gender = function(str) gender = str end
-	self.set_first_name = function(str) first_name = str end
-	self.set_last_name = function(str) last_name = str end
-	self.set_middle_name = function(str) middle_name = str end
-	self.set_date_of_birth = function(str) date_of_birth = str end
+	self.set_unique_id = function(num) properties.unique_id = num end
+	self.set_gender = function(str) properties.gender = str end
+	self.set_first_name = function(str) properties.first_name = str end
+	self.set_last_name = function(str) properties.last_name = str end
+	self.set_middle_name = function(str) properties.middle_name = str end
+	self.set_date_of_birth = function(str) properties.date_of_birth = str end
 
 	-- public methods
 	
 	self.show = function()
-		print("**Idenity**: " 
-        .. "ID: " .. unique_id 
-		.. ", First Name: " .. first_name 
-		.. ", Middle Name: " .. middle_name
-		.. ", Last Name: " .. last_name 
-		.. ", Date of Birth: " .. date_of_birth 
-		.. ", Gender: " .. gender)
+        local json = Json.Encode(properties)
+        print(json)
 	end
 		
 	return self -- return the object!
